339180193fa26fdc74561c41d3dcc23268bc6085
Ednesor <edgar.funes2001@gmail.com>
2023-07-20 19:00:50 -0300

    feat: fix

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const INITIAL_STATE = [
     country: "United Kingdom",
     lat: 51.52,
     lon: -0.11,
-    temp_c: 18,
+    temp_c: 18.4,
     condition: {
       text: "Partly cloudy",
       icon: "//cdn.weatherapi.com/weather/64x64/night/116.png",
@@ -20,9 +20,10 @@ const INITIAL_STATE = [
     wind_kph: 11.9,
     wind_dir: "SW",
     humidity: 88,
+    localtime: "2023-07-14 23:32",
   },
   {
-    name: "London",
+    name: "Argetnina",
     region: "City of London, Greater London",
     country: "United Kingdom",
     lat: 51.52,
@@ -35,17 +36,89 @@ const INITIAL_STATE = [
     wind_kph: 11.9,
     wind_dir: "SW",
     humidity: 88,
+    localtime: "2023-07-14 23:32",
+  },
+  {
+    name: "Argetnina",
+    region: "City of London, Greater London",
+    country: "United Kingdom",
+    lat: 51.52,
+    lon: -0.11,
+    temp_c: 18,
+    condition: {
+      text: "Partly cloudy",
+      icon: "//cdn.weatherapi.com/weather/64x64/night/116.png",
+    },
+    wind_kph: 11.9,
+    wind_dir: "SW",
+    humidity: 88,
+    localtime: "2023-07-14 23:32",
+  },
+  {
+    name: "Argetnina",
+    region: "City of London, Greater London",
+    country: "United Kingdom",
+    lat: 51.52,
+    lon: -0.11,
+    temp_c: 18,
+    condition: {
+      text: "Partly cloudy",
+      icon: "//cdn.weatherapi.com/weather/64x64/night/116.png",
+    },
+    wind_kph: 11.9,
+    wind_dir: "SW",
+    humidity: 88,
+    localtime: "2023-07-14 23:32",
+  },
+  {
+    name: "Argetnina",
+    region: "City of London, Greater London",
+    country: "United Kingdom",
+    lat: 51.52,
+    lon: -0.11,
+    temp_c: 18,
+    condition: {
+      text: "Partly cloudy",
+      icon: "//cdn.weatherapi.com/weather/64x64/night/116.png",
+    },
+    wind_kph: 11.9,
+    wind_dir: "SW",
+    humidity: 88,
+    localtime: "2023-07-14 23:32",
   },
 ];
 
 function App() {
-  const [WeatherArray, setWeatherArray] = useState<Array<WeatherFromApi>>(INITIAL_STATE);
+  const [WeatherArray, setWeatherArray] =
+    useState<Array<WeatherFromApi>>(INITIAL_STATE);
+
+  const handleAdd = () => {
+    setWeatherArray([
+      ...WeatherArray,
+      {
+        name: "España",
+        region: "City of Espania, Greater Español",
+        country: "United Espain",
+        lat: 51.52,
+        lon: -0.11,
+        temp_c: 18,
+        condition: {
+          text: "Partly cloudy",
+          icon: "//cdn.weatherapi.com/weather/64x64/night/116.png",
+        },
+        wind_kph: 11.9,
+        wind_dir: "SW",
+        humidity: 88,
+        localtime: "2023-07-14 23:32",
+      },
+    ]);
+  };
 
   return (
     <div className="App">
       <Menu />
       <Routes>
-        <Route path="/" element={<Cards />} />
+        <Route path="/" element={<Cards weather={WeatherArray} />} />
         <Route path="/details" element={<Details />} />
       </Routes>
     </div>
